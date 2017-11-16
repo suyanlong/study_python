97c1e60ef553e1b06d2a16114f9dfb4355a38dc9
suyanlong <592938410@qq.com>
2017-11-16 16:17:12 +0800

    study setup and click modules

---
+++ examples/setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python2
+# -*- coding:utf-8 -*-
+
+
+from distutils.core import setup
+
+setup(name = 'study_time',
+      version = '1.0',
+      py_modules = ['study_time'],
+      )
--- examples/study_click.py
+++ examples/study_click.py
@@ -10,5 +10,32 @@ def func(param):
     print param
 
 
+@click.command()
+@click.option('--rate', type = float, help = 'rate vale')
+def show(rate):
+    click.echo('rate :%s' % rate)
+
+
+# if __name__ == '__main__':
+    # func()
+    # show()
+
+
+@click.command()
+@click.option('--password', prompt = True, hide_input = True, confirmation_prompt = True)
+def input_password(password):
+    click.echo('password: %s' % password)
+
+
+# if __name__ == '__main__':
+#     input_password()
+
+
+@click.command()
+@click.option('--name', help='The person to greet.')
+def hello(name):
+    click.secho('Hello %s!' % name, fg='red', underline=True)
+    click.secho('Hello %s!' % name, fg='yellow', bg='black')
+
 if __name__ == '__main__':
-    func()
+    hello()
