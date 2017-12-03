a7a9488053f392288a0ab8ac8bd5d9e5f9920887
suyanlong <592938410@qq.com>
2017-12-03 16:54:02 +0800

    study make wheel and tar.gz
    Signed-off-by: suyanlong <592938410@qq.com>

--- setup.py
+++ setup.py
@@ -17,4 +17,11 @@ setup(
     ''',
 )
 
-# pip install --editable .
+# pip install --editable . 生成命令
+# python setup.py sdist  打成tar.gz包
+# python setup.py bdist_wheel 打成whl包
+# python setup.py sdist bdist_wheel 一起打whl，tar.gz包
+
+# tar.gz 与 whl包之间怎么转化？
+# pip wheel leveldb-0.194.tar.gz  => leveldb-0.194-cp27-cp27mu-linux_x86_64.whl
+# pip wheel [*.tar.gz] 就可以了转换为whl包了。
